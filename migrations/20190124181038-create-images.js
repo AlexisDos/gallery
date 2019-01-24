ba60c3620001be396f3f16300552ef834cd0ac40
Luis Castro <luiisenriquecastro@gmail.com>
2019-01-24 11:11:12 -0700

    MIGRATIONS

---
+++ migrations/20190124181038-create-images.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Images', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      server: {
+        type: Sequelize.STRING
+      },
+      url: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      review: {
+        type: Sequelize.STRING
+      },
+      facebook: {
+        type: Sequelize.STRING
+      },
+      status: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Images');
+  }
+};
---
+++ models/images.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Images = sequelize.define('Images', {
+    name: DataTypes.STRING,
+    server: DataTypes.STRING,
+    url: DataTypes.STRING,
+    email: DataTypes.STRING,
+    review: DataTypes.STRING,
+    facebook: DataTypes.STRING,
+    status: DataTypes.INTEGER
+  }, {});
+  Images.associate = function(models) {
+    // associations can be defined here
+  };
+  return Images;
+};
