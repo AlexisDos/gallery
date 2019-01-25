240330991ff6eb7a83b424f500825cc6f20953b2
Luis Castro <luiisenriquecastro@gmail.com>
2019-01-25 14:13:45 -0700

    session

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,23 +2,41 @@
 
 
 const passport = require('passport');
+const express = require('express');
+const session = require('express-session');
 require('./_passport.js');
+
 
 module.exports = {
   login(req, res, next){
     passport.authenticate('local', async (err, user, info) => {   
     try{
-        if(err) throw new Error(err);
-        if(info) throw new Error(info.message);
+        if(err) {throw new Error(err)};
+        if(info){ 
+            await res.status(401).json({message: "Incorrect username or password"});
+        }
+        
 
+
+
+        let app = express()
+        app.use(session({secret:  'secretsession'}));
+
+        // console.log(err);
+        // console.log(info.message);
         const response = await {
-            "isAuth": true,
-            "message": "These credentials has been validated."
+                id: user.id,
+                username: user.username,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                isAuth: true,
+                message: "These credentials has been validated."
         };
-
         await res.status(201).json(response);
+        
     }catch(err){
         next(err);
+        //console.log(err)
     }   
     })(req, res, next)
   }
